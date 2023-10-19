41c947d0c42c2640d3aab8199218a47a18a86d76
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-10-19 11:57:13 +0300

    Added: Tweets' Rolling Averages!

---
+++ DataLemur-SQL-Challenges/Medium/Tweets' Rolling Averages.sql
@@ -0,0 +1,8 @@
+SELECT user_id,
+       tweet_date,
+       ROUND(
+              AVG(tweet_count) 
+              OVER(PARTITION BY user_id ORDER BY tweet_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+              , 2
+           )
+FROM tweets;
