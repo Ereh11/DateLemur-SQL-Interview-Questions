6a8b88a56030b0bcd4aa6d5258f84ee11f411e9c
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2024-02-14 07:42:48 +0200

    Update Page With No Likes.sql

--- DataLemur-SQL-Challenges/Easy/Page With No Likes.sql
+++ DataLemur-SQL-Challenges/Easy/Page With No Likes.sql
@@ -4,3 +4,11 @@ WHERE P.page_id NOT IN
   (
     SELECT PL.page_id FROM page_likes PL
   );
+
+-- Another Solution
+SELECT page_id
+FROM pages
+EXCEPT
+SELECT page_id
+FROM page_likes
+ORDER BY page_id
